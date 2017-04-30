cd46b792e8d279d46bcf95feb0ff8eddd0e6a3c5
john <545244819@qq.com>
2017-04-30 13:57:54 +0800

    类别页面数据

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,6 @@
 class CategoriesController < ApplicationController
-
+  def show
+    @category = Category.find(params[:id])
+    @articles = Article.limit(12).order(created_at: :desc).where({ category_id: @category.id })
+  end
 end
