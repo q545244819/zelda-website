f5b10aec18d0092e645e18bd3935d99594a8dc0b
john <545244819@qq.com>
2017-04-30 13:44:16 +0800

    articles 新增一行 image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429134447) do
+ActiveRecord::Schema.define(version: 20170430053729) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170429134447) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
     t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
