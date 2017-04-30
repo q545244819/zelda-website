ea70407447632789ed5298a887374f00f905db34
john <545244819@qq.com>
2017-04-30 13:30:30 +0800

    区分首页导航和其他页面导航

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,14 @@ class HomeController < ApplicationController
   def index
     @categories = Category.all
     @articles = {}
-    @count = 6
+    @name = 'home'
 
-    @articles['hot'] = Article.limit(@count).order(created_at: :desc)
+    count = 6    
+
+    @articles['hot'] = Article.limit(count).order(created_at: :desc)
     
     @categories.each do |item|
-      @articles[item.id] = Article.limit(@count).order(created_at: :desc).where({ category_id: item.id })
+      @articles[item.id] = Article.limit(count).order(created_at: :desc).where({ category_id: item.id })
     end
   end
 end
