742d382993240d3a4a1a8ec761a2776a11d6ba39
john <545244819@qq.com>
2017-05-03 19:15:14 +0800

    修改 puma 配置文件

--- config/puma_production.rb
+++ config/puma_production.rb
@@ -9,11 +9,11 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 8080 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -45,3 +45,10 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+#后台运行
+daemonize true
+on_restart do
+  puts 'On restart...'
+end
+preload_app!
