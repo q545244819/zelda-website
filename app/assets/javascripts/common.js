6fb0429580ea78668754bd765028d54aad1203b0
john <545244819@qq.com>
2017-05-03 14:50:31 +0800

    添加返回顶部功能

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,9 +1,17 @@
 $(function () {
   var $topbar = $('#js-topbar')
+  var $totop = $('#js-totop')
+  var windowsHeight = $(window).height()
   var lastScrollTop = 0
 
   $(window).on('scroll', function (e) {
     var st = $(this).scrollTop()
+
+    if (st > 0) {
+      $totop.show()
+    } else {
+      $totop.hide()
+    }
 
     if (st > lastScrollTop ) {
       $topbar.addClass('hide')
@@ -12,5 +20,13 @@ $(function () {
     }
 
     lastScrollTop = st
+  })
+
+  $totop.on('click', function (e) {
+    e.preventDefault()
+
+    $('html, body').animate({ scrollTop: 0 }, 'fast')
+
+    return false
   })
 })
